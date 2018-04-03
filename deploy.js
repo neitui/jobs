cc389a940ed06e8c3bd7d5d5caa4241f74226ec8
Jiangtao <321jiangtao@gmail.com>
2018-04-03 20:07:42 +0800

    docs: update jobs

--- deploy.js
+++ deploy.js
@@ -57,26 +57,26 @@ function generateReadme(list) {
 
   const input = `# 内推那点事
 
-  ## 声明
+## 声明
 
-  本repo创建，为了方便 【内推那点事】群里朋友招人。请创建issue自觉遵守格式，方便定时同步到 neitui-sth 公众号。如招聘结束，请关闭issue
+本repo创建，为了方便 【内推那点事】群里朋友招人。请创建issue自觉遵守格式，方便定时同步到 neitui-sth 公众号。如招聘结束，请关闭issue
 
-  由于放到 github 上，如果 邮箱，姓名等信息比较私密，请使用 专用招聘邮箱，昵称等。
+由于放到 github 上，如果 邮箱，姓名等信息比较私密，请使用 专用招聘邮箱，昵称等。
 
-  ${content}
+${content}
 
-  ## 微信群
+## 微信群
 
-  内推那点事，加群请扫下列二维码
+内推那点事，加群请扫下列二维码
 
-  ![内推那点事](./img/wechat.png)
+![内推那点事](./img/wechat.png)
 
-  为证明您是互联网从业者，请出示 github，个人博客，公众号 等。
+为证明您是互联网从业者，请出示 github，个人博客，公众号 等。
 
 
-  ## 公众号
+## 公众号
 
-  微信搜索 neitui-sth ，点击关注。希望您招人/找工作 更简单，高效。
+微信搜索 neitui-sth ，点击关注。希望您招人/找工作 更简单，高效。
   `
 
   return writeFile(path.resolve(process.cwd(), './README.md'), input).then(function() {
